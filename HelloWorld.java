eefeda65d93e90d1a027f16ac95ca6d1e1ff7a60
Omar <martinez.trujillo.omar@gmail.com>
2020-09-21 20:14:17 +0200

    al suelo

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ class HelloWorld
 {
     public static void main(String args[])
     {
-        System.out.println("Hello World");
+		System.out.println("Hikari no hashi wo koete, namida to ai wo toki hanashite");
     }
 }
